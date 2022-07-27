a40bdbaac744d9fdef828d505ea6d2a1f8c15de7
Luis Fernando Zerna Ramos <luis_zerna@hotmail.com>
2022-07-26 22:04:35 -0500

    q

--- AppJFalarmante/Datos/NodoDatos.cs
+++ AppJFalarmante/Datos/NodoDatos.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entidad;
-using System.Text.Json;
 using System.Net.Http;
 using FireSharp.Interfaces;
 using FireSharp.Config;
